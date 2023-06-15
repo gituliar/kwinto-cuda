eb7231a47442669a469450f0a36518c46c6c40c4
Oleksandr Gituliar <oleksandr@gituliar.net>
2023-06-15 17:22:46 +0200

    Delete src/kwVersion.cpp

--- src/kwVersion.cpp
+++
@@ -1,7 +0,0 @@
-#include "kwVersion.h"
-const std::string kw::Version::BUILD_DATE="2023-06-13 22:27:57 +0200";
-const std::string kw::Version::GIT_BRANCH="main";
-const std::string kw::Version::GIT_DATE="2023-06-12 22:20:38 +0200";
-const std::string kw::Version::GIT_DESCRIBE="v0.1.0-3-g2a05d1a+";
-const std::string kw::Version::GIT_REV="2a05d1a+";
-const std::string kw::Version::GIT_TAG="";
